6bb53c8b3de3ff3349449518e564ac7d80c1cc9b
Jaewoong Ryu <woong137137@gmail.com>
2023-12-19 01:00:54 +0900

    Refactor code: Add Print function and update CMakeLists.txt

---
+++ application/printing_hello_world/src/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdint>
+#include <string>
+
+#include "utils/utils.hpp"
+
+int32_t main() {
+    Print("Hello, World!");
+    return 0;
+}
--- module/utils/src/utils.cpp
+++ module/utils/src/utils.cpp
@@ -1,1 +1,8 @@
 #include "utils/utils.hpp"
+#include <iostream>
+
+using namespace std;
+
+void Print(const string& message) {
+    cout << message << endl;
+}
